09aa2ce586953f5dd4cf75b5686fd1d9ad9255db
Nitin Bhadauria <nitinb@cloudstaff.com>
2024-07-10 14:25:12 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ module "labels" {
 
   enabled     = var.enable
   name        = var.name
-  repository  = var.repository
+  repository  = ""
   environment = var.environment
   label_order = var.label_order
-  managedby   = var.managedby
+  managedby   = ""
   extra_tags  = var.extra_tags
 }
 
